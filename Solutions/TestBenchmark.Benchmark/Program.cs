8fd7956b5dd4b1a730f64eb0639f0b9ba1589fc2
Ian Griffiths <ian@interact-sw.co.uk>
2019-09-18 13:29:26 +0100

    Set benchmark results location explicitly

--- Solutions/TestBenchmark.Benchmark/Program.cs
+++ Solutions/TestBenchmark.Benchmark/Program.cs
@@ -1,13 +1,24 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
+using System.IO;
 
 namespace TestBenchmark.Benchmark
 {
     internal static class Program
     {
-        private static void Main()
+        private static void Main(string[] args)
         {
-            BenchmarkRunner.Run<AllBenchmarks>();
+            IConfig config = DefaultConfig.Instance;
+            if (args.Length > 0)
+            {
+                string artifactsPath = args[0];
+                Directory.CreateDirectory(artifactsPath);
+                config = config.WithArtifactsPath(artifactsPath);
+            }
+
+            BenchmarkRunner.Run<AllBenchmarks>(config);
         }
     }
 }
